bf69bf17701598e5e7f3a22dcd46f8a55199b028
bekajunior254 <chanagawaelisha308@gmail.com>
2025-06-05 11:30:40 +0300

    Removing role in my LoginDto

--- backendAPI/DTOs/Logindto.cs
+++ backendAPI/DTOs/Logindto.cs
@@ -5,6 +5,6 @@ namespace School_Management_System.DTOs
         public required string Username { get; set; } = string.Empty;       
          public required string Email { get; set; } = string.Empty;
         public required string Password { get; set; }= string.Empty;
-         public required string Role { get; set; }= string.Empty;
+        
     }
 }
